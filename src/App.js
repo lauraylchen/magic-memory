fa56a1e91305c2edb57324f6386c1e52726ef4aa
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-12 17:55:32 -0400

    Added CSS to body

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className='text-center'>
+    <div className=''>
       <h1>Pokemon Match</h1>
       <button>New Game</button>
     </div>
