53138f5909001723abf4bbad0d377ca79d475f16
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-12 19:55:32 -0400

    Basic styling of cards

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,9 +1,15 @@
 export default function SingleCard({ card }) {
+  const imgClass = "w-full block border-2 border-blue rounded"
+
   return (
-    <div>
+    <div className="relative">
       <div>
-        <img className="" src={card.src} alt="card front" />
-        <img className="" src="/img/cover-illustration-daniel-mackey.png" alt="card back" />
+        <img className={imgClass} src={card.src} alt="card front" />
+        <img
+          className={imgClass}
+          src="/img/cover-illustration-daniel-mackey.png"
+          alt="card back"
+        />
       </div>
     </div>
   )
