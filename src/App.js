5fda7ea211617e19e32873c01e8c33cb26f3b32c
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-12 18:13:12 -0400

    Style button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div className='max-w-4xl mx-auto my-10'>
-      <h1 className="font-black text-4xl">Pokemon Match</h1>
-      <button>New Game</button>
+      <h1 className="font-black text-4xl py-3">Pokemon Match</h1>
+      <button className="btn btn-sm hover:bg-blue hover:text-yellow">New Game</button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': '#637DFF',
+        'yellow': '#FFF9C2',
+      },
+    },
   },
   daisyui: {
     themes: [],
