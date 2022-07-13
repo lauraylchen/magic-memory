c1bb567d9a1fbe6dd41595c4f372272f87f077e4
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-12 23:03:30 -0400

    Handle click on cards and remember the two choices and compare them to see if match

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
@@ -13,18 +13,43 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
-  // Shuffle
+  // Shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
-      .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }))
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({ ...card, id: Math.random() }))
 
     setCards(shuffledCards)
     setTurns(0)
   }
 
-  console.log(cards, turns)
+  // Handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // Compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("Match")
+        resetTurn()
+      } else {
+        console.log("Don't match")
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  // Reset choices & increase turns
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
 
   return (
     <div className='max-w-4xl mx-auto my-10'>
@@ -37,7 +62,11 @@ function App() {
       </button>
       <div className="grid grid-rows-3 grid-flow-col gap-3 mt-10">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+          />
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,5 +1,9 @@
-export default function SingleCard({ card }) {
+export default function SingleCard({ card, handleChoice }) {
   const imgClass = "w-full block border-2 border-blue rounded"
+
+  const handleClick = () => {
+    handleChoice(card)
+  }
 
   return (
     <div className="relative">
@@ -9,6 +13,7 @@ export default function SingleCard({ card }) {
           className={imgClass}
           src="/img/cover-illustration-daniel-mackey.png"
           alt="card back"
+          onClick={handleClick}
         />
       </div>
     </div>
